f66a8c5fe76e5f3a2cb0a6ba6fe1ffb36b2a1bb7
Nelcy Julieth <nelcy.pinto@sofka.com.co>
2020-06-07 10:52:11 -0500

    03-todoapp

---
+++ src/app/app.reducer.ts
@@ -0,0 +1,17 @@
+import { Todo } from './todos/models/todo.model';
+import { ActionReducerMap } from '@ngrx/store';
+
+import { filtrosValidos } from './filtro/filtro.actions';
+import { filtroReducer } from './filtro/filtro.reducer';
+
+import { todoReducer } from './todos/todo.reducer';
+
+export interface AppState {
+  todos: Todo[];
+  filtro: filtrosValidos;
+}
+
+export const appReducers: ActionReducerMap<AppState> = {
+  todos: todoReducer,
+  filtro: filtroReducer,
+};
